317ba67de049bdf38ba5b1802e03a43b8b342124
Jester Temporada <jester.temporada@gmail.com>
2020-05-12 21:37:22 +0800

    Add prettier, update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'compat'],
   rules: {
-    semi: ['error', 'always', { omitLastInOneLineBlock: true }]
-  }
+    'compat/compat': 'warn',
+    'simple-import-sort/sort': 'warn',
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    '@typescript-eslint/no-use-before-define': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
+import purple from '@material-ui/core/colors/purple';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React from 'react';
 
 const theme = createMuiTheme({
   palette: {
     primary: purple,
-    secondary: green
-  }
+    secondary: green,
+  },
 });
 
-function App () {
+function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <Button variant="contained" color="primary">
         Hello World
       </Button>
